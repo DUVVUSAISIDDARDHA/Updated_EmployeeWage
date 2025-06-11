119512623de3322c68590448e211b8eea0a40123
DUVVUSAISIDDARDHA <ds8529@srmist.edu.in>
2025-06-11 21:34:26 +0530

    Updated UC2

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,13 +1,47 @@
+//UC1
 function Check_Employee(){
     let attendance=Math.floor(Math.random()*2);
-    if(attendance==0){
-        return "Employee is Absent";
+    let attend=0
+    if(attendance==1){
+        attend=1;
     }
-    else{
-        return "Employee is Present";
-    }
-
-    //return attendance;
+    return attend;
 }
+let attends=Check_Employee();
+ console.log(attends);
 
-console.log(Check_Employee());
+//UC2
+function worktimes(){
+    let time=Math.floor(Math.random()*3);
+    let worktime=0
+    if(time==1){
+        worktime=1;
+    }
+    else if(time==2){
+        worktime=2;
+    }
+    return worktime;
+}
+let worktype=worktimes();
+console.log(worktype);
+const no_time=0;
+const part_time=1;
+const full_time=2;
+const wage_per_hour=20;
+
+if(attends) {
+    switch (worktype) {
+        case 0:
+            console.log(`The Employee Wage for Zero Working Hours is: ${0 * wage_per_hour}`);
+            break;
+        case 1:
+            console.log(`The Employee Wage for Four Hours is: ${4 * wage_per_hour}`);
+            break;
+        case 2:
+            console.log(`The Employee Wage for 8 Hours is: ${8 * wage_per_hour}`);
+            break;
+    }
+}
+else {
+    console.log("The Employee is Absent");
+}
